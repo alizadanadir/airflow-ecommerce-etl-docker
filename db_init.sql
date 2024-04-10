8571f396cf45fb94333bff73d2392bd9772e4163
Nadir <alizada.nadir24@gmail.com>
2024-04-11 00:48:56 +0400

    added scripts for creating object on warehouse schema

--- db_init.sql
+++ db_init.sql
@@ -35,4 +35,66 @@ CREATE TABLE staging.user_activity_log(
     quantity int8
 ) ;
 
+--
+CREATE TABLE warehouse.d_city (
+	id serial4 NOT NULL,
+	city_id int4 NULL,
+	city_name varchar(50) NULL,
+	CONSTRAINT d_city_city_id_key UNIQUE (city_id),
+	CONSTRAINT d_city_pkey PRIMARY KEY (id)
+);
+CREATE INDEX d_city1 ON warehouse.d_city USING btree (city_id);
 
+CREATE TABLE warehouse.d_customer (
+	id serial4 NOT NULL,
+	customer_id int4 NOT NULL,
+	first_name varchar(15) NULL,
+	last_name varchar(15) NULL,
+	city_id int4 NULL,
+	CONSTRAINT d_customer_customer_id_key UNIQUE (customer_id),
+	CONSTRAINT d_customer_pkey PRIMARY KEY (id)
+);
+CREATE INDEX d_cust1 ON warehouse.d_customer USING btree (customer_id);
+
+CREATE TABLE warehouse.d_item (
+	id serial4 NOT NULL,
+	item_id int4 NOT NULL,
+	item_name varchar(50) NULL,
+	CONSTRAINT d_item_item_id_key UNIQUE (item_id),
+	CONSTRAINT d_item_pkey PRIMARY KEY (id)
+);
+CREATE UNIQUE INDEX d_item1 ON warehouse.d_item USING btree (item_id);
+
+CREATE TABLE warehouse.f_research (
+	date_time timestamp NULL,
+	category_id int4 NULL,
+	geo_id int4 NULL,
+	sales_qty int4 NULL,
+	sales_amt numeric NULL
+);
+
+CREATE TABLE warehouse.f_daily_sales (
+	id serial4 NOT NULL,
+	date_time timestamp NOT NULL,
+	item_id int4 NOT NULL,
+	customer_id int4 NOT NULL,
+	city_id int4 NOT NULL,
+	quantity int8 NULL,
+	payment_amount numeric(10, 2) NULL,
+	CONSTRAINT f_daily_sales_pkey PRIMARY KEY (id)
+);
+CREATE INDEX f_ds1 ON warehouse.f_daily_sales USING btree (date_id);
+CREATE INDEX f_ds2 ON warehouse.f_daily_sales USING btree (item_id);
+CREATE INDEX f_ds3 ON warehouse.f_daily_sales USING btree (customer_id);
+CREATE INDEX f_ds4 ON warehouse.f_daily_sales USING btree (city_id);
+
+ALTER TABLE warehouse.f_daily_sales ADD CONSTRAINT f_daily_sales_customer_id_fkey FOREIGN KEY (customer_id) REFERENCES warehouse.d_customer(customer_id);
+ALTER TABLE warehouse.f_daily_sales ADD CONSTRAINT f_daily_sales_item_id_fkey FOREIGN KEY (item_id) REFERENCES warehouse.d_item(item_id);
+ALTER TABLE warehouse.f_daily_sales ADD CONSTRAINT f_daily_sales_item_id_fkey1 FOREIGN KEY (item_id) REFERENCES warehouse.d_item(item_id);
+
+CREATE TABLE warehouse.f_daily_activity (
+	date_time timestamp NULL,
+	action_id int4 NULL,
+	customer_id int4 NULL,
+	quantity int8 NULL
+);
